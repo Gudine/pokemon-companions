c9d2a565e4ac6b86bec7cf0425e9836cdfb6b31d
Gudine <gudine283@gmail.com>
2025-02-24 21:25:15 -0300

    🚧 creates basic pokémon card

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,12 @@
+import { pokemonList } from "./pokemonList";
+import { SpeciesBig } from "./components/SpeciesBig";
+
 export function App() {
   return (
-    <></>
+    <main class="flex flex-row flex-wrap justify-evenly gap-2 bg-stone-300">
+      { [...pokemonList].slice(0).map(([name]) => (
+        <SpeciesBig name={ name } />
+      ))}
+    </main>
   )
 }
---
+++ src/components/SpeciesBig.tsx
@@ -0,0 +1,62 @@
+import { Dex, SpeciesName } from "@pkmn/dex";
+import { Sprites } from "@pkmn/img";
+import { useEffect, useMemo, useRef, useState } from "preact/hooks";
+
+export function SpeciesBig({ name }: { name: SpeciesName }) {
+  const ref = useRef(null);
+  const [isVisible, setVisible] = useState(false);
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+
+    if (!isVisible && ref.current) {
+      observer = new IntersectionObserver((entries, obs) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            setVisible(entry.isIntersecting);
+            obs.disconnect();
+          }
+        }
+      }, { rootMargin: "100%" });
+
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer?.disconnect();
+    }
+  }, [ref, isVisible, setVisible]);
+
+  const pkmn = useMemo(() => Dex.species.get(name), [name]);
+
+  const image = useMemo(() => Sprites.getPokemon(name, {
+    gen: "gen5",
+    protocol: window.location.protocol.slice(0, -1) as "http" | "https",
+    domain: window.location.host,
+  }), [name]);
+
+  return (
+    <div
+      ref={ref}
+      class="flex flex-col items-center
+        rounded-lg p-2 cursor-pointer
+        bg-stone-50 border-gray-400 border-2
+        hover:bg-stone-200"
+    >
+      <span class="self-start font-bold text-xs">
+        { `#${pkmn.num}` }
+      </span>
+      { isVisible ? <img
+        src={ image.url }
+        width={ image.w }
+        height={ image.h }
+        alt={ pkmn.name }
+        style={ { imageRendering: image.pixelated ? "pixelated" : "auto" } }
+      /> : <div style={ {
+        width: image.w,
+        height: image.h
+      } }/>}
+      <span class="font-bold text-sm">{ pkmn.name }</span>
+    </div>
+  )
+}
