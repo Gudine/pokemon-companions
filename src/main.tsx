0dcbefb91c7291becf713984b8e06a6ce920cbad
Gudine <gudine283@gmail.com>
2025-03-02 14:56:20 -0300

    💄 make all icons inlined

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { render } from 'preact';
+import { IconContext } from 'react-icons';
 import { App } from './app.tsx';
 import './index.css';
 
-render(<App />, document.body);
+render(<IconContext.Provider value={{ className: "inline" }}>
+  <App />
+</IconContext.Provider>, document.body);
