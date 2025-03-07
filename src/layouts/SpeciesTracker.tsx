f2f568e0e430743ca7adf743805e1ba2512f8024
Gudine <gudine283@gmail.com>
2025-03-06 23:25:13 -0300

    🔥 remove button to add Pokémon from SpeciesTracker

--- src/layouts/SpeciesTracker.tsx
+++ src/layouts/SpeciesTracker.tsx
@@ -1,12 +1,7 @@
-import { useSignal } from "@preact/signals";
 import { pokemonList } from "../pokemonList";
-import { CornerButton } from "../components/common/CornerButton";
 import { SpeciesBig } from "../components/SpeciesBig";
-import { AddPokemonModal } from "../components/AddPokemonModal";
 
 export function SpeciesTracker() {
-  const showModal = useSignal(false);
-
   return (
     <main class="grid grid-cols-1 grid-rows-1">
       <div class="col-span-full row-span-full flex flex-row flex-wrap justify-evenly gap-2 p-2">
@@ -14,8 +9,6 @@ export function SpeciesTracker() {
           <SpeciesBig key={ name } name={ name } />
         ))}
       </div>
-      <CornerButton onClick={ () => showModal.value = true } />
-      { showModal.value && <AddPokemonModal close={ () => showModal.value = false } />}
     </main>
   )
 }
