1ede7229ad1d1c7f813d34d14d6543e8077391d2
Gudine <gudine283@gmail.com>
2025-03-07 19:10:50 -0300

    🐛 add key to PlaythroughSection components in SavedPlaythroughs

--- src/layouts/SavedPlaythroughs.tsx
+++ src/layouts/SavedPlaythroughs.tsx
@@ -20,7 +20,7 @@ export function SavedPlaythroughs() {
   return (
     <main class="grid grid-cols-1 grid-rows-1">
       <div class="col-span-full row-span-full flex flex-col gap-4 pt-4 pb-4">
-        { playthroughs.value.map((playthrough) => (<PlaythroughSection playthrough={ playthrough }/>))}
+        { playthroughs.value.map((playthrough) => (<PlaythroughSection key={ playthrough.name } playthrough={ playthrough }/>))}
         <button
           class="pl-4 pr-4 pt-2 pb-2
             text-xl bg-indigo-700 text-gray-100 hover:bg-indigo-600
