919d1be90298ffcc375131786ab89f2de62cc6f4
Gudine <gudine283@gmail.com>
2025-03-07 17:19:25 -0300

    ✨ display species tracker differently based on completion

--- src/components/SpeciesBig.tsx
+++ src/components/SpeciesBig.tsx
@@ -4,7 +4,18 @@ import { useMemo } from "preact/hooks";
 import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
 import { defaultGen } from "../data";
 
-export function SpeciesBig({ name }: { name: SpeciesName }) {
+interface Props {
+  name: SpeciesName;
+  completion: number; // between 0 and 1, inclusive
+}
+
+const getCompletionStyle = (completion: number) => completion === 0
+  ? "bg-stone-50 hover:bg-stone-200"
+  : completion === 1
+    ? "bg-indigo-300 hover:bg-indigo-400"
+    : "half-completed"
+
+export function SpeciesBig({ name, completion }: Props) {
   const [isVisible, elemRef] = useIntersectionObserver(true);
 
   const pkmn = useMemo(() => defaultGen.species.get(name)!, [name]);
@@ -18,10 +29,9 @@ export function SpeciesBig({ name }: { name: SpeciesName }) {
   return (
     <div
       ref={elemRef}
-      class="flex flex-col items-center
-        rounded-lg p-2 cursor-pointer
-        bg-stone-50 border-gray-400 border-2
-        hover:bg-stone-200"
+      class={`flex flex-col items-center
+        rounded-lg p-2 cursor-pointer border-2 border-gray-400
+        ${getCompletionStyle(completion)}`}
     >
       <span class="self-start font-bold text-xs">
         { `#${pkmn.num}` }
--- src/layouts/SpeciesTracker.tsx
+++ src/layouts/SpeciesTracker.tsx
@@ -1,12 +1,27 @@
 import { pokemonList } from "../pokemonList";
 import { SpeciesBig } from "../components/SpeciesBig";
+import { useSignal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
+import { PokemonUnit } from "../db/PokemonUnit";
 
 export function SpeciesTracker() {
+  const formSet = useSignal<Set<string>>(new Set());
+
+  useEffect(() => {
+    (async () => {
+      formSet.value = new Set((await PokemonUnit.getAll()).map((pkmn) => pkmn.form));
+    })();
+  }, []);
+
   return (
     <main class="grid grid-cols-1 grid-rows-1">
       <div class="col-span-full row-span-full flex flex-row flex-wrap justify-evenly gap-2 p-2">
-        { [...pokemonList].slice(0,50).map(([name]) => (
-          <SpeciesBig key={ name } name={ name } />
+        { [...pokemonList].slice(0,50).map(([name, forms]) => (
+          <SpeciesBig
+            key={ name }
+            name={ name }
+            completion={ forms.filter((form) => formSet.value.has(form)).length / forms.length }
+          />
         ))}
       </div>
     </main>
