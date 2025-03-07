6673edd4a7a46a4f3128a93785c1434242f0a4ee
Gudine <gudine283@gmail.com>
2025-03-06 23:18:50 -0300

    ✨ display real saved pokémon on "Saved Pokémon" section, and reformats section

---
+++ src/components/PlaythroughSection.tsx
@@ -0,0 +1,58 @@
+import { FaPencil, FaUserPlus, FaChevronDown, FaChevronRight } from "react-icons/fa6";
+import { SpeciesPokemonSmall } from "../components/SpeciesPokemonSmall";
+import { SetUtils } from "../utils/setUtils";
+import { useSignal, useSignalEffect } from "@preact/signals";
+import { IPlaythrough, IPokemonUnit } from "../db/db";
+import { PokemonUnit } from "../db/PokemonUnit";
+
+export function PlaythroughSection({ playthrough }: { playthrough: IPlaythrough }) {
+  const collapsed = useSignal(true);
+  const units = useSignal<IPokemonUnit[]>([]);
+  
+  useSignalEffect(() => {
+    (async () => {
+      if (collapsed.value === false) {
+        units.value = await PokemonUnit.getByPlaythrough(playthrough.name);
+      }
+    })();
+  });
+  
+  return (
+    <section>
+      <header class="bg-indigo-700 text-gray-100
+        text-lg p-1
+        flex flex-row justify-between">
+        <div class="flex flex-row items-center gap-4 pl-2">
+          <button
+            onClick={ () => collapsed.value = !collapsed.value }
+            class="cursor-pointer flex"
+          >
+            { collapsed.value ? <FaChevronRight title="Expand" /> : <FaChevronDown title="Collapse" /> }
+          </button>
+          <h2>[{playthrough.date.toLocaleDateString()}] {playthrough.name}</h2>
+        </div>
+        <div class="pr-4 flex flex-row gap-2 items-center">
+          <button
+            class="cursor-pointer flex"
+          >
+            <FaUserPlus title="Add Pokémon" />
+          </button>
+          <button
+            class="cursor-pointer flex"
+          >
+            <FaPencil title="Edit playthrough" />
+          </button>
+        </div>
+      </header>
+      { !collapsed.value && (
+        <div class="flex flex-row flex-wrap justify-evenly
+        bg-indigo-200 border-indigo-700 border-t-0 border-2 border-dashed
+          gap-2 p-2 pb-6 mb-2">
+          { units.value.map(({ data }) => (
+            <SpeciesPokemonSmall key={ data } pkmn={ SetUtils.unpackSet(data)! } />
+          )) }
+        </div>
+      )}
+    </section>
+  )
+}
--- src/layouts/SavedPlaythroughs.tsx
+++ src/layouts/SavedPlaythroughs.tsx
@@ -1,21 +1,35 @@
 import { useSignal } from "@preact/signals";
-import { pokemonList } from "../pokemonList";
-import { generatePokemon } from "../demoSets";
-import { CornerButton } from "../components/common/CornerButton";
-import { SpeciesPokemonSmall } from "../components/SpeciesPokemonSmall";
+import { useEffect } from "preact/hooks";
+import { Playthrough } from "../db/Playthrough";
+import { IPlaythrough } from "../db/db";
 import { AddPlaythroughModal } from "../components/AddPlaythroughModal";
+import { PlaythroughSection } from "../components/PlaythroughSection";
 
 export function SavedPlaythroughs() {
   const showModal = useSignal(false);
+  const playthroughs = useSignal<IPlaythrough[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      if (showModal.value === false) {
+        playthroughs.value = await Playthrough.getAll();
+      }
+    })();
+  }, [showModal.value]);
 
   return (
     <main class="grid grid-cols-1 grid-rows-1">
-      <div class="col-span-full row-span-full flex flex-row flex-wrap justify-evenly gap-2 p-2">
-        { [...pokemonList].slice(-50).map(([name]) => (
-          <SpeciesPokemonSmall key={ name } pkmn={ generatePokemon(name) } />
-        ))}
+      <div class="col-span-full row-span-full flex flex-col gap-4 pt-4 pb-4">
+        { playthroughs.value.map((playthrough) => (<PlaythroughSection playthrough={ playthrough }/>))}
+        <button
+          class="pl-4 pr-4 pt-2 pb-2
+            text-xl bg-indigo-700 text-gray-100 hover:bg-indigo-600
+            self-center rounded-3xl cursor-pointer"
+          onClick={ () => showModal.value = true }
+        >
+          +
+        </button>
       </div>
-      <CornerButton onClick={ () => showModal.value = true } />
       { showModal.value && <AddPlaythroughModal close={ () => showModal.value = false } />}
     </main>
   )
