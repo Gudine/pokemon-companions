9a5071fa8b40f14920818b424a6279fa6207c3e8
Gudine <gudine283@gmail.com>
2025-03-14 00:16:48 -0300

    🔧 add new @ path alias to root /src folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,12 @@ export default defineConfig({
     preact(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      },
+    ]
+  }
 })
