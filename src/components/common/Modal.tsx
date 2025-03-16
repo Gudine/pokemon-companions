0fd22e261f4f22ca3779449b9a71932a2f92b58d
Gudine <gudine283@gmail.com>
2025-03-15 23:15:45 -0300

    🐛 make Modal work with overflowing content

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -10,9 +10,12 @@ export function Modal({ class: className, close, children }: Props) {
       onClick={(ev) => ev.target === ev.currentTarget && close()}
       class="fixed inset-0 bg-black/40 flex items-center justify-center"
     >
-      <div class={`rounded-4xl ${className}
-        bg-stone-200 border-4 border-stone-500 p-4`}>
-          {children}
+      <div class={`rounded-4xl ${className} bg-stone-200 border-4 border-stone-500 overflow-hidden`}>
+        <div class="overflow-auto w-full h-full">
+          <div class="p-4 min-w-full min-h-full">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   )
