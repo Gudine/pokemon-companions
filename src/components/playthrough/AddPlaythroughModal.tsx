f652e99c2cf70e35cedbc51b9d06a8c87116ef49
Gudine <gudine283@gmail.com>
2025-03-17 22:05:33 -0300

    ✨ add way to delete a playthrough when editing it

--- src/components/playthrough/AddPlaythroughModal.tsx
+++ src/components/playthrough/AddPlaythroughModal.tsx
@@ -6,6 +6,7 @@ import { Modal } from "@/components/common/Modal";
 import { Button } from "@/components/common/Button";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { DatabaseError } from "@/errors";
+import { useSignal } from "@preact/signals";
 
 const GENS = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
 
@@ -24,6 +25,8 @@ export function AddPlaythroughModal({ close, playthrough }: { close: () => void,
       gen: playthrough?.gen ?? GENS[GENS.length - 1],
     },
   });
+
+  const isDeleting = useSignal(false);
 
   const onSubmit: SubmitHandler<Inputs> = async ({ name, date, gen }) => {
     if (playthrough) {
@@ -50,6 +53,18 @@ export function AddPlaythroughModal({ close, playthrough }: { close: () => void,
     }
   }
 
+  const onDelete = async () => {
+    if (confirm(
+      `Are you sure you want to delete the "${playthrough!.name}" playthrough?\n`
+      + "This will also delete all the Pokémon under this playthrough."
+    )) {
+      isDeleting.value = true;
+      await Playthrough.delete(playthrough!.id);
+  
+      close();
+    }
+  };
+
   return (
     <Modal close={ close } class="w-80 h-100">
       <form
@@ -67,7 +82,7 @@ export function AddPlaythroughModal({ close, playthrough }: { close: () => void,
               pt-1 pb-1 pl-2 pr-2
             disabled:bg-gray-300 text-stone-700"
             type="text"
-            disabled={ isSubmitting }
+            disabled={ isSubmitting || isDeleting.value }
             {...register("name", {
               required: "Name can't be empty",
             })}
@@ -83,7 +98,7 @@ export function AddPlaythroughModal({ close, playthrough }: { close: () => void,
               pt-1 pb-1 pl-2 pr-2
             disabled:bg-gray-300 text-stone-700"
             type="date"
-            disabled={ isSubmitting }
+            disabled={ isSubmitting || isDeleting.value }
             {...register("date", {
               valueAsDate: true,
               required: "Date must be set",
@@ -99,18 +114,35 @@ export function AddPlaythroughModal({ close, playthrough }: { close: () => void,
             class="bg-gray-100 border-2 border-stone-500 rounded-lg
               pt-1 pb-1 pl-2 pr-2
             disabled:bg-gray-300 text-stone-700"
-            disabled={ !!playthrough || isSubmitting }
+            disabled={ !!playthrough || isSubmitting || isDeleting.value }
             {...register("gen", { valueAsNumber: true })}
           >
             {GENS.map((genId) => <option value={genId}>{genId}</option>)}
           </select>
         </label>
-        <Button class="self-end" type="submit" disabled={!(isValid || isSubmitting)}>
-          { !isSubmitting
-            ? "Submit"
-            /* @ts-expect-error */
-            : <AiOutlineLoading className="animate-spin" /> }
-        </Button>
+        <div class={`flex ${playthrough ? "justify-between" : "justify-end"}`}>
+          {!!playthrough && <Button
+            class="bg-red-600 border-red-600 hover:text-red-800"
+            type="button"
+            disabled={!isValid || isSubmitting || isDeleting.value}
+            onClick={ onDelete }
+          >
+            { !isDeleting.value
+              ? "Delete"
+              /* @ts-expect-error */
+              : <AiOutlineLoading className="animate-spin" /> }
+          </Button>}
+          <Button
+            class="self-end"
+            type="submit"
+            disabled={!isValid || isSubmitting || isDeleting.value}
+          >
+            { !isSubmitting
+              ? "Save"
+              /* @ts-expect-error */
+              : <AiOutlineLoading className="animate-spin" /> }
+          </Button>
+        </div>
       </form>
     </Modal>
   )
