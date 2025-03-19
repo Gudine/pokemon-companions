6418e02227fd642ac117d91eec27532a9cc82e7d
Gudine <gudine283@gmail.com>
2025-03-19 16:35:00 -0300

    ✨ make getSprites script save all Pokémon sprites to a sheet instead of individually

--- scripts/getSprites.ts
+++ scripts/getSprites.ts
@@ -1,67 +1,106 @@
+import type { Specie } from "@pkmn/data";
 import fs from "fs/promises";
 import { join, dirname } from "path";
 import { setTimeout as sleep } from "timers/promises";
-import { SpeciesName } from "@pkmn/data";
+import Sharp from "sharp";
 import { Sprites, Icons } from "@pkmn/img";
 import { defaultGen } from "../src/data";
 
-async function exists(path: string) {
-  try {
-    await fs.stat(path);
-    return true;
-  } catch (err) {
-    return false;
-  }
-}
+const imagesData = new Map<string, Buffer | undefined>();
 
-const invalidURLs: string[] = [];
+const plainImages: string[] = [];
+const shinyImages: string[] = [];
 
 async function downloadImage(url: string) {
   const pathname = new URL(url).pathname;
   const outPath = join("public", pathname);
 
-  if (await exists(outPath)) return;
   console.log(`Downloading ${url}...`);
-
   const fetched = await fetch(url);
-  if (!fetched.ok) {
-    invalidURLs.push(pathname);
-    return;
-  }
+  if (!fetched.ok) throw new Error(`File ${url} not found`);
 
-  const imgData = await fetched.arrayBuffer();
+  const imgData = await fetched.bytes();
 
   await fs.mkdir(dirname(outPath), { recursive: true });
   await fs.writeFile(outPath, Buffer.from(imgData));
   await sleep(100);
 }
 
-async function downloadImages(name: SpeciesName) {
-  const species = defaultGen.species.get(name)!;
+async function getImage(url: string): Promise<string | undefined> {
+  const pathname = new URL(url).pathname;
+  if (imagesData.has(pathname)) return pathname;
 
-  try {
-    await downloadImage(Sprites.getPokemon(species.name, { gen: "gen5" }).url);
-    await downloadImage(Sprites.getPokemon(species.name, { gen: "gen5", shiny: true }).url);
-    await downloadImage(Icons.getPokemon(species.name).url)
-    
-    if (!species.gender) for (const gender of ["M", "F"] as const) {
-      await downloadImage(Sprites.getPokemon(species.name, { gen: "gen5", gender }).url);
-      await downloadImage(Sprites.getPokemon(species.name, { gen: "gen5", gender, shiny: true }).url);
-      await downloadImage(Icons.getPokemon(species.name, { gender }).url);
-    }
-  } catch (err) {
-    throw new Error(`Image not found for ${name}`);
+  // Check if file exists in system
+  const existingFile = await fs.readFile(join("public", pathname)).catch(() => undefined);
+  if (existingFile) {
+    imagesData.set(pathname, existingFile);
+    return pathname;
+  }
+
+  // Download from server
+  console.log(`Downloading ${url}...`);
+  const fetched = await fetch(url);
+  if (!fetched.ok) {
+    imagesData.set(pathname, undefined);
+    return ;
+  }
+
+  imagesData.set(pathname, Buffer.from(await fetched.bytes()));
+  return sleep(100, pathname);
+}
+
+async function getImages(species: Specie) {
+  const plainPath = await getImage(Sprites.getPokemon(species.name, { gen: 5 }).url);
+  if (plainPath && !plainImages.includes(plainPath)) plainImages.push(plainPath);
+
+  const shinyPath = await getImage(Sprites.getPokemon(species.name, { gen: 5, shiny: true }).url);
+  if (shinyPath && !shinyImages.includes(shinyPath)) shinyImages.push(shinyPath);
+  
+  if (!species.gender) for (const gender of ["M", "F"] as const) {
+    const plainGenderPath = await getImage(Sprites.getPokemon(species.name, { gen: 5, gender }).url);
+    if (plainGenderPath && !plainImages.includes(plainGenderPath)) plainImages.push(plainGenderPath);
+
+    const shinyGenderPath = await getImage(Sprites.getPokemon(species.name, { gen: 5, gender, shiny: true }).url);
+    if (shinyGenderPath && !shinyImages.includes(shinyGenderPath)) shinyImages.push(shinyGenderPath);
   }
 }
 
 for (const species of defaultGen.species) {
-  await downloadImages(species.name);
+  await getImages(species);
 }
 
-for (const item of defaultGen.items) {
-  await downloadImage(Icons.getItem(item.name).url);
+await downloadImage(Sprites.getSubstitute({ gen: 5 }).url);
+await downloadImage(Icons.getPokemon("Zubat").url);
+await downloadImage(Icons.getItem("Potion").url);
+
+// -----------------------------------------------
+const GRID_WIDTH = 40;
+
+const nameToComposite = (plainImage: string, i: number) => {
+  const data = imagesData.get(plainImage);
+  if (!data) return;
+
+  return {
+    input: data,
+    left: (i % GRID_WIDTH) * 96,
+    top: Math.floor(i / GRID_WIDTH) * 96,
+  }
 }
 
-await downloadImage(Sprites.getSubstitute({ gen: "gen5" }).url);
+await Sharp({ create: {
+  width: GRID_WIDTH * 96,
+  height: Math.ceil(plainImages.length / GRID_WIDTH) * 96,
+  channels: 4,
+  background: "#00000000",
+} }).composite(plainImages.map(nameToComposite).filter((v) => v !== undefined))
+  .webp({ lossless: true }).toFile("public/sprites/pokemon-sheet.webp");
 
-fs.writeFile(join("src", "invalidImages.json"), JSON.stringify(invalidURLs));
+await Sharp({ create: {
+  width: GRID_WIDTH * 96,
+  height: Math.ceil(shinyImages.length / GRID_WIDTH) * 96,
+  channels: 4,
+  background: "#00000000",
+} }).composite(shinyImages.map(nameToComposite).filter((v) => v !== undefined))
+  .webp({ lossless: true }).toFile("public/sprites/pokemonshiny-sheet.webp");
+
+await fs.writeFile(join("src", "imageIndices.json"), JSON.stringify([plainImages, shinyImages]));
