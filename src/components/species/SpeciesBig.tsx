ae31b4868084d6aeca188f0be5fdda500927a26b
Gudine <gudine283@gmail.com>
2025-03-26 16:21:24 -0300

    ✨ add modal to see units of a given species

--- src/components/species/SpeciesBig.tsx
+++ src/components/species/SpeciesBig.tsx
@@ -1,6 +1,9 @@
 import type { SpeciesName } from "@pkmn/data";
 import { defaultGen } from "@/data";
 import { ImgUtils } from "@/utils/imgUtils";
+import { useSignal } from "@preact/signals";
+import { createPortal } from "preact/compat";
+import { SpeciesModal } from "./SpeciesModal";
 
 interface Props {
   name: SpeciesName;
@@ -14,24 +17,33 @@ const getCompletionStyle = (completion: number) => completion === 0
     : "half-completed"
 
 export function SpeciesBig({ name, completion }: Props) {
+  const showModal = useSignal(false);
+
   const pkmn = defaultGen.species.get(name)!;
   const image = ImgUtils.getPokemon(name);
 
   return (
-    <div
-      class={`flex flex-col items-center
-        rounded-lg p-2 cursor-pointer border-2 border-gray-400
-        ${getCompletionStyle(completion)}`}
-    >
-      <span class="self-start font-bold text-xs">
-        { `#${pkmn.num}` }
-      </span>
-      <span
-        role="img"
-        aria-label={ pkmn.name }
-        style={ image.css }
-      />
-      <span class="font-bold text-sm">{ pkmn.name }</span>
-    </div>
+    <>
+      <div
+        class={`flex flex-col items-center
+          rounded-lg p-2 cursor-pointer border-2 border-gray-400
+          ${getCompletionStyle(completion)}`}
+        onClick={() => showModal.value = true}
+      >
+        <span class="self-start font-bold text-xs">
+          { `#${pkmn.num}` }
+        </span>
+        <span
+          role="img"
+          aria-label={ pkmn.name }
+          style={ image.css }
+        />
+        <span class="font-bold text-sm">{ pkmn.name }</span>
+      </div>
+      {showModal.value && createPortal(<SpeciesModal
+        close={ () => showModal.value = false }
+        speciesName={ name }
+      />, document.body)}
+    </>
   )
 }
---
+++ src/components/species/SpeciesModal.tsx
@@ -0,0 +1,71 @@
+import type { SpeciesName } from "@pkmn/data";
+import { PokemonUnit } from "@/db/PokemonUnit";
+import { useDBResource } from "@/hooks/useDBResource";
+import { pokemonList } from "@/pokemonList";
+import { ImgUtils } from "@/utils/imgUtils";
+import { unpackSet } from "@/utils/setUtils";
+import { GenProvider } from "@/contexts/GenContext";
+import { Playthrough } from "@/db/Playthrough";
+import { Placeholder } from "@/components/common/Placeholder";
+import { Modal } from "@/components/common/Modal";
+import { PokemonSmall } from "@/components/pokemon/PokemonSmall";
+
+interface Props {
+  close: () => void;
+  speciesName: SpeciesName;
+}
+
+export function SpeciesModal({ close, speciesName }: Props) {
+  return (
+    <Modal close={ close } class="w-9/10 h-9/10">
+      <SpeciesModalInner close={ close } speciesName={ speciesName } />
+    </Modal>
+  )
+}
+
+function SpeciesModalInner({ speciesName }: Props) {
+  const units = useDBResource(
+    () => PokemonUnit.getBySpecies(speciesName),
+    "pkmn",
+    { species: speciesName },
+  );
+
+  const playthroughGens = useDBResource(
+    async () => new Map((await Playthrough.getAll()).map((curr) => [curr.id, curr.gen])),
+    "playthrough",
+    {},
+  );
+
+  const forms = pokemonList.get(speciesName) ?? [];
+  const formImages = forms.map((form) => ImgUtils.getPokemon(form));
+
+  return (
+    <div class="flex flex-row h-full">
+      {forms.map((form, i) => <div class="grow flex flex-col gap-0 bg-stone-200 border-dotted not-last:border-r-2">
+        <div class="flex flex-col pl-0.5 pr-0.5 bg-linear-to-b from-transparent from-75% to-stone-400/65">
+          <div class="grow flex flex-col items-center text-center pr-1.5 pl-1.5 pb-2
+            border-dashed border-b-2">
+            <span
+              role="img"
+              aria-label={ form }
+              style={ formImages[i].css }
+            />
+            <span class="font-bold font-kodchasan text-md">{ form }</span>
+          </div>
+        </div>
+        <div class="grow flex flex-col justify-center overflow-y-auto">
+          <div class="p-2 gap-2 flex flex-row flex-wrap justify-around overflow-y-auto overflow-x-hidden">
+            { units.filter((unit) => unit.form === form).length === 0 && <Placeholder /> }
+            { units.filter((unit) => unit.form === form)
+              .map((info) => ({ ...info, set: unpackSet(info.data, playthroughGens.get(info.playthrough) ?? 9) }))
+              .filter(({ set }) => set !== undefined)
+              .map(({ id, set, playthrough }) => (
+                <GenProvider gen={ playthroughGens.get(playthrough) ?? 9 }>
+                  <PokemonSmall key={ id } pkmn={ set! } />
+                </GenProvider>))}
+          </div>
+        </div>
+      </div>)}
+    </div>
+  )
+}
