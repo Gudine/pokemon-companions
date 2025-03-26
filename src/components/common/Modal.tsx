8dfd067bd4114094b47e47bb7c0a40b823338627
Gudine <gudine283@gmail.com>
2025-03-26 15:36:33 -0300

    ✨ make page unscrollable when any modal is active

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -11,7 +11,7 @@ export function Modal({ class: className, close, children }: Props) {
   return (
     <div
       onClick={(ev) => ev.target === ev.currentTarget && close()}
-      class="fixed inset-0 bg-black/40 flex items-center justify-center"
+      class="modal fixed inset-0 bg-black/40 flex items-center justify-center"
     >
       <div class={`rounded-4xl ${className} bg-stone-200 border-4 border-stone-500 overflow-hidden`}>
         <div class="overflow-auto w-full h-full grid grid-cols-1 grid-rows-1">
