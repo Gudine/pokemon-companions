d25648794f63c843fc0ad8901c39101a0e9a6e17
Gudine <gudine283@gmail.com>
2025-03-28 18:49:15 -0300

    📱 adapt Header font size to screen size

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <header class="text-center text-indigo-700 p-2 font-kodchasan">
-      <h1 class="font-bold text-5xl">Pokémon Companion Tracker</h1>
-      <p class="text-lg">To immortalize all your favorite Pokémon</p>
+      <h1 class="font-bold text-2xl md:text-5xl">Pokémon Companion Tracker</h1>
+      <p class="text-sm md:text-lg">To immortalize all your favorite Pokémon</p>
     </header>
   )
 }
