143c97f64afe5d4b600f45b3b205ddad74d462ea
Gudine <gudine283@gmail.com>
2025-05-07 16:37:33 -0300

    ✨ add Toaster component to App root

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { Header } from "./layouts/Header";
 import { Navigation } from "./layouts/Navigation";
 import { Main } from "./layouts/Main";
 import { Footer } from "./layouts/Footer";
+import { Toaster } from "react-hot-toast";
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
       <Navigation />
       <Main />
       <Footer />
+      <Toaster position="bottom-right" />
     </div>
   )
 }
