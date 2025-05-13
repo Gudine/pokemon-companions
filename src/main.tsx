1f15c3015607d22251a11b5b2dc3014da4e7c679
Gudine <gudine283@gmail.com>
2025-05-13 16:13:34 -0300

    ➕ ♻️ replace use of Google Fonts with Fontsource dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { render } from 'preact';
 import { IconContext } from 'react-icons';
 import { App } from './app.tsx';
 import './index.css';
+import '@fontsource/kodchasan';
+import '@fontsource/kodchasan/700';
+import '@fontsource-variable/nunito';
 
 render(<IconContext.Provider value={{ className: "inline" }}>
   <App />
