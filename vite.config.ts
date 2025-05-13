9f9ef8a3ed2d98d69da480d30281edaa3ba9b4e7
Gudine <gudine283@gmail.com>
2025-05-13 16:47:18 -0300

    ✨ make website work as a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,39 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'url'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     preact(),
     tailwindcss(),
+    VitePWA({
+      manifestFilename: "manifest.json",
+      workbox: { globPatterns: ['**/*.{js,css,html,png,svg,webp,woff,woff2}'] },
+      manifest: {
+        name: "Pokémon Companion Tracker",
+        short_name: "Poké-Companions",
+        description: "App to keep track of important Pokémon through all your playthroughs",
+        start_url: "./",
+        display: "standalone",
+        theme_color: "#372aac",
+        background_color: "#e7e5e5",
+        icons: [
+          {
+            src: "icon.png",
+            type: "image/png",
+            sizes: "512x512"
+          },
+          {
+            src: "icon.svg",
+            type: "image/svg+xml",
+            sizes: "any"
+          }
+        ]
+      }
+    }),
   ],
   build: {
     // For top-level await
