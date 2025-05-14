aedb634b16ea7fbdcebc23e0efcd39b1b2ead36c
Gudine <gudine283@gmail.com>
2025-05-14 14:26:04 -0300

    🐛 make unobtainable data show up in data sources

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,12 @@ import { Dex } from "@pkmn/dex";
 function existsFn(d: Data) {
   if (!d.exists) return false;
   if (d.kind === "Ability" && d.id === "noability") return false;
-  if ('isNonstandard' in d && d.isNonstandard && d.isNonstandard !== "Past") return false;
+  if (
+    'isNonstandard' in d
+    && d.isNonstandard
+    && d.isNonstandard !== "Past"
+    && d.isNonstandard !== "Unobtainable"
+  ) return false;
   if (d.kind === "Species" && (d.battleOnly || d.forme === "Gmax")) return false;
   
   return true;
